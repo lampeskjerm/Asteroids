74d8cbb1a55a192aea8051c517f1a8a60818099d
lampeskjerm <slkajmeprem@gmail.com>
2025-11-10 17:27:28 +0100

    Added fps

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import pygame
-pygame.init()
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
 
@@ -8,6 +7,11 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     
+    pygame.init()
+    
+    clock = pygame.time.Clock()
+    dt = 0
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True:
@@ -17,6 +21,7 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
